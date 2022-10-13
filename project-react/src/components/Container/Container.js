e8df2f98aac5eefda84a1d3413dcd957daefd51a
Marek Skolik <marek250296@interia.pl>
2022-10-13 16:39:54 +0200

    first task

---
+++ project-react/src/components/Container/Container.js
@@ -0,0 +1,9 @@
+import styles from './Container.module.scss';
+
+const Container = props => {
+  return (
+    <container className={styles.container}>{props.children}</container>
+  );
+};
+
+export default Container;
--- project-react/src/components/List/List.js
+++ project-react/src/components/List/List.js
@@ -1,4 +1,5 @@
 import styles from './List.module.scss';
+import Column from '../Column/Column';
 
 const List = () => {
   return (
@@ -7,16 +8,10 @@ const List = () => {
         <h2 class={styles.title}>Things to do<span>soon</span></h2>
       </header>
       <p class={styles.description}>Interesting things I want to check out</p>
-      <section class={styles.columns}>
-        <article>
-          <h2>Books</h2>
-        </article>
-        <article>
-          <h2>Movies</h2>
-        </article>
-        <article>
-          <h2>Games</h2>
-        </article>
+      <section className={styles.columns}>
+        <Column title="Books" icon="book" />
+        <Column title="Movies" icon="film" />
+        <Column title="Games" icon="gamepad" />
       </section>
     </div>
     );
