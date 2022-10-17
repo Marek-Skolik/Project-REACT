50d885ab27325992f951f69dc37a0c89ced36f51
Marek Skolik <marek250296@interia.pl>
2022-10-17 10:03:17 +0200

    commit

--- project-react/src/components/Column/Column.js
+++ project-react/src/components/Column/Column.js
@@ -1,4 +1,5 @@
 import styles from './Column.module.scss'
+import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
 
 const Column = props => {
@@ -6,7 +7,7 @@ const Column = props => {
     <article className={styles.column}>
       <h2 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h2>
       <ul className={styles.cards}>
-        {props.cards.map(card => <li key={card.id}>{card.title}</li>)}
+        {props.cards.map(card => <Card key={card.id} title={card.title} />)}
       </ul>
       <CardForm columnId={props.id} action={props.addCard} />
     </article>
