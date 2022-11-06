28d5ab9d8b9a577e892fbf141f6d7fed8eb2d22e
Marek Skolik <marek250296@interia.pl>
2022-11-06 19:29:56 +0100

    nav

---
+++ project-react/src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import styles from './NavBar.module.scss';
+import Container from '../Container/Container';
+
+const NavBar =props => {
+    return (
+        <nav className={styles.NavBar}>
+            <Container>
+                <div className={styles.navigation}>
+                    <a className={styles.icon} href="/"><span className="fa fa-tasks" /></a>
+                    <ul>
+                        <li><a href="/">HOME</a></li>
+                        <li><a href="/favorite">FAVORITE</a></li>
+                        <li><a href="/about">ABOUT</a></li>
+                    </ul>
+                </div>
+            </Container>    
+        </nav>
+    )
+}
+
+export default NavBar;
