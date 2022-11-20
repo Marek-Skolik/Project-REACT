6f7b92b3f237a5efe2c952e67c303bdcee8ed8d4
Marek Skolik <marek250296@interia.pl>
2022-11-20 17:16:51 +0100

    commit

--- project-react/src/components/List/List.js
+++ project-react/src/components/List/List.js
@@ -2,17 +2,18 @@ import styles from './List.module.scss';
 import Column from '../Column/Column';
 import ColumnForm from '../ColumnForm/ColumnForm';
 import { useSelector } from 'react-redux';
-import { getAllColumns, getListById } from '../../redux/store'; 
+import { getListById } from '../../redux/store'; 
+import { getColumnsByList } from '../../redux/store'; 
  
 const List = () => {
 
-  const columns = useSelector(getAllColumns);
+  const columns = useSelector(getColumnsByList);
   const listData = useSelector((state) => getListById(state));
 
   return (
     <div className={styles.list}>
       <header className={styles.header}>
-        <h2 className={styles.title}>Things to do<span>soon!</span></h2>
+        <h2 className={styles.title}>{listData.title}</h2>
       </header>
       <p className={styles.description}>Interesting things I want to check out</p>
       <section className={styles.columns}>
--- project-react/src/redux/store.js
+++ project-react/src/redux/store.js
@@ -11,6 +11,8 @@ export const getAllColumns = state => state.columns;
 
 export const getListById = ({ lists }, listId ) => lists.find((list) => list.id === listId);
 
+export const getColumnsByList = ({ columns }, listId) => columns.filter((column) => column.listId === listId);
+
 // action creators
 export const addColumn = payload => ({ type: 'ADD_COLUMN', payload });
 
